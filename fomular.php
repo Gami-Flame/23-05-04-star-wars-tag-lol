6d5cd713d7216534b9ed120eaa06f30bfad32ac4
Gami-Flame <124242991+Gami-Flame@users.noreply.github.com>
2023-05-09 09:23:41 +0200

    datei erstellt

---
+++ fomular.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    echo $_GET["ereignis"];
+    echo $_GET["datum"];
+    ?>
+
+</body>
+</html>
